4c3ea756091d0f51c8015c0e067c6e1e9f5d4d05
Muhammad Nadzim <nadzim13.work@gmail.com>
2024-12-24 01:04:00 +0800

    add css, js, about.html, contact.html, indext.html

---
+++ css/style.cc
@@ -0,0 +1,15 @@
+<style>
+    section#about {
+        background-color: #eaeaea;
+        padding: 2rem;
+        margin-top: 2rem;
+        border-radius: 8px;
+    }
+    section#about h2 {
+        color: #333;
+    }
+    section#about p {
+        font-size: 1rem;
+        line-height: 1.6;
+    }
+</style>
